6cc7ded604ea733c7b1a3fda624b8237b60d822f
Dino Utjesinovic <dino@ri-pos.hr>
2018-02-20 14:16:43 +0100

    Prije eject

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ class App extends Component {
     person.name = event.target.value;
     const persons = [...this.state.persons];
     persons[personIndex] = person;
-    this.setState({persons: persons});
+    this.setState({ persons: persons });
   }
 
   togglePersonHandler = () => {
@@ -59,7 +59,8 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
@@ -81,15 +82,24 @@ class App extends Component {
           })}
         </div>
       );
+      style.backgroundColor = 'red';
+    }
+
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold');
     }
 
     return (
       <div className="App">
         <h1>Hi, I'm ReactApp</h1>
-        <p>This is really working!</p>
+        <p className={classes.join(' ')}>This is really working!</p>
         <button
           style={style}
-          onClick={this.togglePersonHandler}>Switch Name</button>
+          onClick={this.togglePersonHandler}>Toggle Persons</button>
         {persons}
       </div>
     );
